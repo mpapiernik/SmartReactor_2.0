bd4e28c82ffc48e7d51ddeaed69ac678052cce48
m.papiernik <m.kwidzinska@smartmedia.com.pl>
2019-09-25 08:44:27 +0200

    git pages

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,11 @@
 var requireDir = require('require-dir');
 
 requireDir('./build', {recurse: true});
+
+var gulp = require('gulp');
+var ghPages = require('gulp-gh-pages');
+ 
+gulp.task('deploy', function() {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages());
+});
